f069a5b913362658a345384073776dc5d0d6b4ab
KidDamper <midejamiu2023@gmail.com>
2026-09-16 21:27:31 -0400

    Centralize core bot constants

---
+++ src/core/constants.js
@@ -0,0 +1,10 @@
+export const BOT={
+  name:'THE DAMPER_BOT',
+  creatorId:'000001',
+  currency:'Damper Coins',
+  startingCoins:500,
+  dailyCoins:200,
+  maxRpgLevel:50
+};
+
+export const ACCESS_CHANNELS=['@nah_idmeme','@Updamper_bot'];
